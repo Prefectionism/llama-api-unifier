79c6313b5f30304d7b8e715c6654e587b48e78ac
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-05-23 14:32:52 +0000

    modify typo

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -12,4 +12,16 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/sashabaranov/
+	"github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	urlFlag := flag.String("url", "http://localhost:8080/oai/v1", "server url")
+	tokenFlag := flag.String("token", "", "server token")
+	modelFlag := flag.String("model", "", "model id")
+
+	flag.Parse()
+
+	ctx := context.Background()
+
+	reader := b
