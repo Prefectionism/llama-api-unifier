5a7a9c1d93cc6798107effeb5eafc1c31ad880cf
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-06-12 16:34:41 +0000

    release v0.2.3

--- config/config_extractors.go
+++ config/config_extractors.go
@@ -7,4 +7,10 @@ import (
 	"github.com/adrianliechti/llama/pkg/extractor"
 	"github.com/adrianliechti/llama/pkg/extractor/code"
 	"github.com/adrianliechti/llama/pkg/extractor/tesseract"
-	"
+	"github.com/adrianliechti/llama/pkg/extractor/text"
+	"github.com/adrianliechti/llama/pkg/extractor/unstructured"
+)
+
+func (cfg *Config) RegisterExtractor(model string, e extractor.Provider) {
+	if cfg.extractors == nil {
+		cfg.extractors = make(map[strin
