75288c2e946b862c6948428df3bcb192fc317127
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-06-21 18:16:42 +0000

    Update docker files

--- config/config_file.go
+++ config/config_file.go
@@ -11,4 +11,22 @@ import (
 )
 
 func parseFile(path string) (*configFile, error) {
-	data, err := os.ReadFile(pa
+	data, err := os.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	data = []byte(os.ExpandEnv(string(data)))
+
+	var config configFile
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+type configFile struct {
+	Authorizers []authorizerConfig 
