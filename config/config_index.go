aff1401e76eaff495fe3a23c26de5ebd6c7121e0
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-07-09 17:55:20 +0000

    - ‘tidal/.#TempoOSC.hs’ / ‘tidal/TempoOSC.hs’

--- config/config_index.go
+++ config/config_index.go
@@ -1,4 +1,11 @@
 package config
 
 import (
-	
+	"errors"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/index"
+	"github.com/adrianliechti/llama/pkg/index/aisearch"
+	"github.com/adrianliechti/llama/pkg/index/bing"
+	"github.com/adrianliechti/llama/pkg/index/chroma"
+	"github.com/adrianliechti/llama/pkg/index/custom"
