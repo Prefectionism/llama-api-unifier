d5076c2133186e66c9cb986851fee5e8b6bec742
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-07-10 09:55:38 +0000

    Add rvmrc

--- config/config_index.go
+++ config/config_index.go
@@ -9,3 +9,8 @@ import (
 	"github.com/adrianliechti/llama/pkg/index/bing"
 	"github.com/adrianliechti/llama/pkg/index/chroma"
 	"github.com/adrianliechti/llama/pkg/index/custom"
+	"github.com/adrianliechti/llama/pkg/index/duckduckgo"
+	"github.com/adrianliechti/llama/pkg/index/elasticsearch"
+	"github.com/adrianliechti/llama/pkg/index/memory"
+	"github.com/adrianliechti/llama/pkg/index/tavily"
+	"github.com/adrianliechti/llama/pkg/index/wea
