bc2679ff9bf5cc269457b7231ff63cd1215d922e
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-07-20 17:23:15 +0000

    updatez

--- config/config_provider.go
+++ config/config_provider.go
@@ -45,3 +45,260 @@ func (cfg *Config) RegisterCompleter(model string, c provider.Completer) {
 
 	cfg.completer[model] = c
 }
+
+func (cfg *Config) RegisterSynthesizer(model string, s provider.Synthesizer) {
+	cfg.RegisterModel(model)
+
+	if cfg.synthesizer == nil {
+		cfg.synthesizer = make(map[string]provider.Synthesizer)
+	}
+
+	cfg.synthesizer[model] = s
+}
+
+func (cfg *Config) RegisterTranslator(model string, t provider.Translator) {
+	cfg.RegisterModel(model)
+
+	if cfg.translator == nil {
+		cfg.translator = make(map[string]provider.Translator)
+	}
+
+	cfg.translator[model] = t
+}
+
+func (cfg *Config) RegisterTranscriber(model string, t provider.Transcriber) {
+	cfg.RegisterModel(model)
+
+	if cfg.transcriber == nil {
+		cfg.transcriber = make(map[string]provider.Transcriber)
+	}
+
+	cfg.transcriber[model] = t
+}
+
+func (cfg *Config) RegisterRenderer(model string, r provider.Renderer) {
+	cfg.RegisterModel(model)
+
+	if cfg.renderer == nil {
+		cfg.renderer = make(map[string]provider.Renderer)
+	}
+
+	cfg.renderer[model] = r
+}
+
+func (cfg *Config) registerProviders(f *configFile) error {
+	for _, p := range f.Providers {
+		for id, m := range p.Models {
+			r, err := createProvider(p, m.ID)
+
+			if err != nil {
+				return err
+			}
+
+			if embedder, ok := r.(provider.Embedder); ok {
+				cfg.RegisterEmbedder(id, embedder)
+			}
+
+			if completer, ok := r.(provider.Completer); ok {
+				if m.Adapter != "" {
+					adapter, err := createCompleterAdapter(m.Adapter, completer)
+
+					if err != nil {
+						return err
+					}
+
+					completer = adapter
+				}
+
+				cfg.RegisterCompleter(id, completer)
+			}
+
+			if synthesizer, ok := r.(provider.Synthesizer); ok {
+				cfg.RegisterSynthesizer(id, synthesizer)
+			}
+
+			if translator, ok := r.(provider.Translator); ok {
+				cfg.RegisterTranslator(id, translator)
+			}
+
+			if transcriber, ok := r.(provider.Transcriber); ok {
+				cfg.RegisterTranscriber(id, transcriber)
+			}
+
+			if renderer, ok := r.(provider.Renderer); ok {
+				cfg.RegisterRenderer(id, renderer)
+			}
+		}
+	}
+
+	return nil
+}
+
+func createProvider(cfg providerConfig, model string) (any, error) {
+	switch strings.ToLower(cfg.Type) {
+
+	case "anthropic":
+		return anthropicProvider(cfg, model)
+
+	case "automatic1111":
+		return automatic1111Provider(cfg, model)
+
+	case "huggingface":
+		return huggingfaceProvider(cfg, model)
+
+	case "langchain":
+		return langchainProvider(cfg, model)
+
+	case "llama":
+		return llamaProvider(cfg, model)
+
+	case "ollama":
+		return ollamaProvider(cfg, model)
+
+	case "openai":
+		return openaiProvider(cfg, model)
+
+	case "mistral":
+		return mistralProvider(cfg, model)
+
+	case "groq":
+		return groqProvider(cfg, model)
+
+	case "coqui":
+		return coquiProvider(cfg)
+
+	case "mimic":
+		return mimicProvider(cfg)
+
+	case "whisper":
+		return whisperProvider(cfg)
+
+	case "azure-translator":
+		return azuretranslatorProvider(cfg, model)
+
+	case "deepl":
+		return deeplProvider(cfg, model)
+
+	case "custom":
+		return customProvider(cfg, model)
+
+	default:
+		return nil, errors.New("invalid provider type: " + cfg.Type)
+	}
+}
+
+func anthropicProvider(cfg providerConfig, model string) (*anthropic.Client, error) {
+	var options []anthropic.Option
+
+	// if cfg.URL != "" {
+	// 	options = append(options, openai.WithURL(cfg.URL))
+	// }
+
+	if cfg.Token != "" {
+		options = append(options, anthropic.WithToken(cfg.Token))
+	}
+
+	if model != "" {
+		options = append(options, anthropic.WithModel(model))
+	}
+
+	return anthropic.New(options...)
+}
+
+func automatic1111Provider(cfg providerConfig, model string) (*automatic1111.Client, error) {
+	var options []automatic1111.Option
+
+	if cfg.URL != "" {
+		options = append(options, automatic1111.WithURL(cfg.URL))
+	}
+
+	return automatic1111.New(options...)
+}
+
+func huggingfaceProvider(cfg providerConfig, model string) (*huggingface.Client, error) {
+	var options []huggingface.Option
+
+	if cfg.Token != "" {
+		options = append(options, huggingface.WithToken(cfg.Token))
+	}
+
+	return huggingface.New(cfg.URL, options...)
+}
+
+func langchainProvider(cfg providerConfig, model string) (*langchain.Client, error) {
+	var options []langchain.Option
+
+	// if model != "" {
+	// 	options = append(options, langchain.WithModel(model))
+	// }
+
+	return langchain.New(cfg.URL, options...)
+}
+
+func llamaProvider(cfg providerConfig, model string) (*llama.Client, error) {
+	var options []llama.Option
+
+	if model != "" {
+		options = append(options, llama.WithModel(model))
+	}
+
+	return llama.New(cfg.URL, options...)
+}
+
+func ollamaProvider(cfg providerConfig, model string) (*ollama.Client, error) {
+	var options []ollama.Option
+
+	if model != "" {
+		options = append(options, ollama.WithModel(model))
+	}
+
+	return ollama.New(cfg.URL, options...)
+}
+
+func openaiProvider(cfg providerConfig, model string) (*openai.Client, error) {
+	var options []openai.Option
+
+	if cfg.URL != "" {
+		options = append(options, openai.WithURL(cfg.URL))
+	}
+
+	if cfg.Token != "" {
+		options = append(options, openai.WithToken(cfg.Token))
+	}
+
+	if model != "" {
+		options = append(options, openai.WithModel(model))
+	}
+
+	return openai.New(options...)
+}
+
+func mistralProvider(cfg providerConfig, model string) (*mistral.Client, error) {
+	var options []mistral.Option
+
+	if cfg.Token != "" {
+		options = append(options, mistral.WithToken(cfg.Token))
+	}
+
+	if model != "" {
+		options = append(options, mistral.WithModel(model))
+	}
+
+	return mistral.New(options...)
+}
+
+func groqProvider(cfg providerConfig, model string) (*groq.Client, error) {
+	var options []groq.Option
+
+	if cfg.Token != "" {
+		options = append(options, groq.WithToken(cfg.Token))
+	}
+
+	if model != "" {
+		options = append(options, groq.WithModel(model))
+	}
+
+	return groq.New(options...)
+}
+
+func coquiProvider(cfg providerConfig) (*coqui.Client, error) {
