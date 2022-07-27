3eb533f9eda6330fa81ae0a5a5fc4a7a6aed5a06
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-07-27 21:24:08 +0000

    sorting fix

--- config/config_tools.go
+++ config/config_tools.go
@@ -7,4 +7,16 @@ import (
 	"github.com/adrianliechti/llama/pkg/index"
 	"github.com/adrianliechti/llama/pkg/provider"
 	"github.com/adrianliechti/llama/pkg/tool"
-	"github
+	"github.com/adrianliechti/llama/pkg/tool/custom"
+	"github.com/adrianliechti/llama/pkg/tool/search"
+)
+
+func (c *Config) RegisterTool(id string, val tool.Tool) {
+	if c.tools == nil {
+		c.tools = make(map[string]tool.Tool)
+	}
+
+	c.tools[id] = val
+}
+
+type toolContext struct {
