13692e2f56ff48bbde2b5012135e39bbfc3d2e7a
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-08-12 09:01:38 +0000

    New packages

--- examples/custom-chat/main.go
+++ examples/custom-chat/main.go
@@ -6,4 +6,22 @@ import (
 	"strings"
 	"time"
 
-	"github.com/adrianliech
+	"github.com/adrianliechti/llama/pkg/provider/custom"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := &Server{}
+
+	server := grpc.NewServer()
+
+	custom.RegisterCompleterServer(server, s)
+
+	if err := server.Serve(l
