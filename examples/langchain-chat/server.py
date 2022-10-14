cc1df0434f4bcdae0f88cece2491d1ec21a0b50f
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-10-14 20:45:23 +0000

    Working on AOServ 2.0

--- examples/langchain-chat/server.py
+++ examples/langchain-chat/server.py
@@ -1,1 +1,11 @@
-#!/usr/bin/env p
+#!/usr/bin/env python
+import os
+import uvicorn
+
+from typing import Any, List, Union
+from fastapi import FastAPI
+from langserve import add_routes
+
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import
