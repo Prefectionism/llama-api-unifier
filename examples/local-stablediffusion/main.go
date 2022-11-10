12021ca1da5de7ad4715fdee672af0b3cbdb283a
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2022-11-10 19:44:47 +0000

    Update 1-col-portfolio.css

--- examples/local-stablediffusion/main.go
+++ examples/local-stablediffusion/main.go
@@ -2,4 +2,17 @@ package main
 
 import (
 	"context"
-	"encodi
+	"encoding/base64"
+	"os"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	config := openai.DefaultConfig("")
+	config.BaseURL = "http://localhost:8080/oai/v1"
+
+	model := "stable-diffusion"
+	prompt := "a cute baby sea otter"
+
+	client := openai.NewClie
