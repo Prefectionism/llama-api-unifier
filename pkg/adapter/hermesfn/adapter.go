45fdf2b21701f87d2c9d79d8906291eb8c24b2af
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-01-05 10:17:46 +0000

    updating gems

--- pkg/adapter/hermesfn/adapter.go
+++ pkg/adapter/hermesfn/adapter.go
@@ -7,4 +7,15 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/adrianliechti/llama/pkg/a
+	"github.com/adrianliechti/llama/pkg/adapter"
+	"github.com/adrianliechti/llama/pkg/provider"
+)
+
+var _ adapter.Provider = &Adapter{}
+
+// https://github.com/NousResearch/Hermes-Function-Calling
+type Adapter struct {
+	completer provider.Completer
+}
+
+func New(completer provider.Completer) (*Ad
