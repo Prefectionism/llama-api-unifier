62ccc4ed7d306d18d7ea3c69db03dfb4290dd28e
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-02-11 17:22:37 +0000

    Added logout

--- pkg/chain/assistant/chain.go
+++ pkg/chain/assistant/chain.go
@@ -1,1 +1,18 @@
 package assistant
+
+import (
+	"context"
+	"errors"
+	"slices"
+
+	"github.com/adrianliechti/llama/pkg/chain"
+	"github.com/adrianliechti/llama/pkg/prompt"
+	"github.com/adrianliechti/llama/pkg/provider"
+)
+
+var _ chain.Provider = &Chain{}
+
+type Chain struct {
+	completer provider.Completer
+
+	template 
