714d6b2e8c1b50f65537a7ce43d67620a1dac0da
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-02-12 21:40:25 +0000

    net: compat: Ignore MSG_CMSG_COMPAT in compat_sys_{send, recv}msg

--- pkg/chain/assistant/chain.go
+++ pkg/chain/assistant/chain.go
@@ -15,4 +15,19 @@ var _ chain.Provider = &Chain{}
 type Chain struct {
 	completer provider.Completer
 
-	template 
+	template *prompt.Template
+	messages []provider.Message
+
+	temperature *float32
+}
+
+type Option func(*Chain)
+
+func New(options ...Option) (*Chain, error) {
+	c := &Chain{}
+
+	for _, option := range options {
+		option(c)
+	}
+
+	if c.compl
