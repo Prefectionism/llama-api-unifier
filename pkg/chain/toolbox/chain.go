e1b6456ab5472103e2c980bf2ae53e07fc0f7145
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-02-17 17:36:20 +0000

    Fix a stupid typo

--- pkg/chain/toolbox/chain.go
+++ pkg/chain/toolbox/chain.go
@@ -7,4 +7,21 @@ import (
 	"slices"
 
 	"github.com/adrianliechti/llama/pkg/chain"
-	"github.com/adrianliechti/llama/p
+	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/adrianliechti/llama/pkg/to"
+	"github.com/adrianliechti/llama/pkg/tool"
+)
+
+var _ chain.Provider = &Chain{}
+
+type Chain struct {
+	completer provider.Completer
+
+	tools map[string]tool.Tool
+
+	temperature *float32
+}
+
+type Option func(*Chain)
+
+func New(o
