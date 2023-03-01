e340b7606c446300818cf3fdcc8cdfefb016286c
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-03-01 13:25:47 +0000

    more photos

--- pkg/extractor/tesseract/client.go
+++ pkg/extractor/tesseract/client.go
@@ -85,3 +85,93 @@ func (c *Client) Extract(ctx context.Context, input extractor.File, options *ext
 
 	if err != nil {
 		return nil, err
+	}
+
+	if _, err := io.Copy(file, input.Content); err != nil {
+		return nil, err
+	}
+
+	w.Close()
+
+	req, _ := http.NewRequestWithContext(ctx, "POST", url, &b)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, convertError(resp)
+	}
+
+	var data Result
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	if data.Data.Stderr != "" {
+		return nil, errors.New(data.Data.Stderr)
+	}
+
+	result := extractor.Document{
+		Name: input.Name,
+	}
+
+	output := data.Data.Stdout
+	output = strings.ReplaceAll(output, "\r\n", "\n")
+
+	var lines []string
+
+	for _, line := range strings.Split(output, "\n") {
+		line = strings.TrimLeft(line, ". ")
+		lines = append(lines, line)
+	}
+
+	splitter := text.NewSplitter()
+	splitter.ChunkSize = c.chunkSize
+	splitter.ChunkOverlap = c.chunkOverlap
+
+	chunks := splitter.Split(strings.Join(lines, "\n"))
+
+	for i, chunk := range chunks {
+		chunk = strings.ReplaceAll(chunk, "\n\n", "\n")
+
+		block := []extractor.Block{
+			{
+				ID:      fmt.Sprintf("%s#%d", result.Name, i),
+				Content: chunk,
+			},
+		}
+
+		result.Blocks = append(result.Blocks, block...)
+	}
+
+	return &result, nil
+}
+
+func (c *Client) optionsJSON() string {
+	options := map[string]any{
+		"languages": []string{
+			"eng",
+			"deu",
+		},
+	}
+
+	value, _ := json.Marshal(options)
+	return string(value)
+}
+
+func convertError(resp *http.Response) error {
+	data, _ := io.ReadAll(resp.Body)
+
+	if len(data) == 0 {
+		return errors.New(http.StatusText(resp.StatusCode))
+	}
+
+	return errors.New(string(data))
+}
---
+++ pkg/extractor/tesseract/models.go
@@ -0,0 +1,10 @@
+package tesseract
+
+type Result struct {
+	Data struct {
+		// Exit struct {
+		// 	Code   int `json:"code"`
+		// 	Signal any `json:"signal"`
+		// } `json:"exit"`
+
+		Stderr string `
