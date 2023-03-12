89f45a89340ca17029d6e40faf309104bf0b4db1
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-03-12 08:09:52 +0000

    Update badge

--- pkg/index/aisearch/models.go
+++ pkg/index/aisearch/models.go
@@ -1,3 +1,25 @@
 package aisearch
 
-t
+type Results struct {
+	Value []Result `json:"value"`
+}
+
+type Result map[string]any
+
+func (r Result) String(name string) string {
+	val, ok := r[name]
+
+	if !ok {
+		return ""
+	}
+
+	data, ok := val.(string)
+
+	if !ok {
+		return ""
+	}
+
+	return data
+}
+
+func (r Result) ID() string
