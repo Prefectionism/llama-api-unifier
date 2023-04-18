d362c901b163630c552acdc67e15c252f8ac6d05
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-04-18 14:57:04 +0000

    requirements updated

--- pkg/index/chroma/models.go
+++ pkg/index/chroma/models.go
@@ -1,4 +1,13 @@
 package chroma
 
 type collection struct {
-	ID string 
+	ID string `json:"id,omitempty"`
+
+	Tenant   string `json:"tenant,omitempty"`
+	Database string `json:"database,omitempty"`
+
+	Name     string         `json:"name,omitempty"`
+	Metadata map[string]any `json:"metadata,omitempty"`
+}
+
+type embeddings struc
