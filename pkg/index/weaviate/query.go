8ca2ec11de1138adbab39e03f2e14670d4078fe5
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-06-02 12:56:14 +0000

    Create Licence.md

--- pkg/index/weaviate/query.go
+++ pkg/index/weaviate/query.go
@@ -9,4 +9,21 @@ import (
 var (
 	//go:embed query.tmpl
 	queryTemplateText string
-	queryTemplate     = template.Must(template.New("query"
+	queryTemplate     = template.Must(template.New("query").Parse(queryTemplateText))
+)
+
+type queryData struct {
+	Class string
+
+	Query  string
+	Vector []float32
+
+	Limit *int
+	Where map[string]string
+}
+
+func executeQueryTemplate(data queryData) string {
+	var buffer bytes.Buffer
+	queryTemplate.Execute(&buffer, data)
+
+	r
