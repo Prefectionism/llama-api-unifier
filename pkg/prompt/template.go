a7fcd2ad07df5e61c2a66de512fb1954ee2299e6
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-06-09 11:09:43 +0000

    ARM: dts: imx25: Fix PWM "per" clocks

--- pkg/prompt/template.go
+++ pkg/prompt/template.go
@@ -10,4 +10,22 @@ type Template struct {
 }
 
 func MustTemplate(text string) *Template {
-	prompt, err := N
+	prompt, err := NewTemplate(text)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return prompt
+}
+
+func NewTemplate(text string) (*Template, error) {
+	tmpl, err := template.New("prompt").Parse(text)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Template{
+		tmpl: tmpl,
+	}
