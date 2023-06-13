58e1cec0b6549467c9d672d519955c9547d505ab
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-06-13 21:04:36 +0000

    Updated Avalonia

--- pkg/provider/anthropic/completer.go
+++ pkg/provider/anthropic/completer.go
@@ -25,4 +25,16 @@ type Completer struct {
 func NewCompleter(options ...Option) (*Completer, error) {
 	c := &Config{
 		url:    "https://api.anthropic.com",
-		client: http.DefaultCli
+		client: http.DefaultClient,
+	}
+
+	for _, option := range options {
+		option(c)
+	}
+
+	return &Completer{
+		Config: c,
+	}, nil
+}
+
+func (c *Completer) Complete(ctx context.Context, messages []provider.Message, options *provider.CompleteOptions) (*provider.Completion, er
