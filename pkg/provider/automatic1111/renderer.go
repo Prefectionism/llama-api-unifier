0db53b971e770ceb581e5458824220517ffcc50d
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-07-19 21:35:55 +0000

    Nightly Build.

--- pkg/provider/automatic1111/renderer.go
+++ pkg/provider/automatic1111/renderer.go
@@ -1,3 +1,20 @@
 package automatic1111
 
-i
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/google/uuid"
+)
+
+var _ provider.Renderer = (*Renderer)(nil)
+
+typ
