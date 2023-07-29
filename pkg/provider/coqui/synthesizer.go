bbc25c4c9a7c9d3a32ee68e7ac1fbd8e34bc261a
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-07-29 12:11:42 +0000

    tty: atmel_serial_probe(): index of atmel_ports[] fix

--- pkg/provider/coqui/synthesizer.go
+++ pkg/provider/coqui/synthesizer.go
@@ -2,4 +2,21 @@ package coqui
 
 import (
 	"context"
-	"
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/google/uuid"
+)
+
+var (
+	_ provider.Synthesizer = (*Synthesizer)(nil)
+)
+
+type Synthesizer struct {
+	*Config
+}
+
+func NewSynthesizer(url string, options ...Option) (*Synthesi
