aeef0381d4146819f80e57fb0f5baae3f60f3f99
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-08-11 08:42:26 +0000

    pipenv update

--- pkg/provider/custom/completer_grpc.pb.go
+++ pkg/provider/custom/completer_grpc.pb.go
@@ -14,4 +14,12 @@ import (
 )
 
 // This is a compile-time assertion to ensure that this generated file
-// is compa
+// is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
+const _ = grpc.SupportPackageIsVersion7
+
+const (
+	Completer_Complete_FullMethodName = "/completer.completer/Complete"
+)
+
+// CompleterClient is the client 
