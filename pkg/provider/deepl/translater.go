b283c5fdde9a31709e089e92cbf8d0d6c9a9d47e
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-09-03 07:37:44 +0000

    Remove comment

--- pkg/provider/deepl/translater.go
+++ pkg/provider/deepl/translater.go
@@ -4,3 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/google/uuid"
+)
+
+type Translator struct {
+	*Config
+}
+
+func NewTranslator(url string, options ...Option) (*Translator, error) {
+	if url == "" {
+		url = "https
