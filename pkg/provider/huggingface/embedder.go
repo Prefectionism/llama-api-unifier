e9420907a219dac667299b6ab6b8b87b1833eceb
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-10-19 19:30:52 +0000

    Added more to readme

--- pkg/provider/huggingface/embedder.go
+++ pkg/provider/huggingface/embedder.go
@@ -2,4 +2,20 @@ package huggingface
 
 import (
 	"context"
-	"encodi
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+)
+
+var _ provider.Embedder = (*Embedder)(nil)
+
+type Embedder struct {
+	*Config
+}
+
+func NewEmbedder(url string, options ...Option) (*Embedder, error) {
+	if url 
