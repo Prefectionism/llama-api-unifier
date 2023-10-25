5f81aaf1e0e5a650cc6771960c75ae34d1f487bd
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-10-25 05:44:45 +0000

    payment

--- pkg/provider/langchain/client.go
+++ pkg/provider/langchain/client.go
@@ -3,4 +3,17 @@ package langchain
 import (
 	"errors"
 	"io"
-	"
+	"net/http"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+)
+
+type Client struct {
+	provider.Completer
+}
+
+func New(url string, options ...Option) (*Client, error) {
+	c, err := NewCompleter(url, options...)
+
+	if err != nil {
+		return nil, 
