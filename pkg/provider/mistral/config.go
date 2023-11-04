34fc36fb2b71c43218d3dd359b245396867b4d8b
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-11-04 22:20:10 +0000

    [maven-release-plugin] prepare release 1.4

--- pkg/provider/mistral/config.go
+++ pkg/provider/mistral/config.go
@@ -1,3 +1,21 @@
 package mistral
 
-import
+import (
+	"net/http"
+)
+
+type Config struct {
+	url string
+
+	token string
+	model string
+
+	client *http.Client
+}
+
+type Option func(*Config)
+
+func WithClient(client *http.Client) Option {
+	return func(c *Config) {
+		c.client = client
+	}
