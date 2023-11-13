219fc478e23cbc74fd2feafa2fa1ddf6b2a89f74
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-11-13 20:58:24 +0000

    Salvaguarda

--- pkg/provider/openai/completer.go
+++ pkg/provider/openai/completer.go
@@ -1,1 +1,19 @@
-package op
+package openai
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+var _ provider.Completer = (*Completer)(nil)
+
+type Completer struct {
+	*Config
+	client *openai.Client
