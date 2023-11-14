40dd6252846c22b9f433d7caf8ec0b744898edc4
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-11-14 22:01:27 +0000

    Translated using Weblate (Simplified Chinese)

--- pkg/provider/openai/completer.go
+++ pkg/provider/openai/completer.go
@@ -17,3 +17,21 @@ var _ provider.Completer = (*Completer)(nil)
 type Completer struct {
 	*Config
 	client *openai.Client
+}
+
+func NewCompleter(options ...Option) (*Completer, error) {
+	cfg := &Config{
+		model: openai.GPT3Dot5Turbo,
+	}
+
+	for _, option := range options {
+		option(cfg)
+	}
+
+	return &Completer{
+		Config: cfg,
+		client: cfg.newClient(),
+	}, nil
+}
+
+f
