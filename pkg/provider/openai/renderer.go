c1f66330f21ac6c0c1ddb870560429f4805d69eb
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-12-08 11:18:19 +0000

    Pontoon: Update Spanish (es) localization of Thimble

--- pkg/provider/openai/renderer.go
+++ pkg/provider/openai/renderer.go
@@ -4,4 +4,18 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
-	"er
+	"errors"
+	"io"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/google/uuid"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+var _ provider.Renderer = (*Renderer)(nil)
+
+type Renderer struct {
+	*Config
+	client *openai.Client
