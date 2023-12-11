41b7f0c7ff08447a2b4c55933c217452490ba826
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-12-11 12:27:26 +0000

    testing new completion code

--- pkg/provider/openai/synthesizer.go
+++ pkg/provider/openai/synthesizer.go
@@ -1,1 +1,16 @@
-pack
+package openai
+
+import (
+	"context"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+
+	"github.com/google/uuid"
+	"github.com/sashabaranov/go-openai"
+)
+
+var _ provider.Synthesizer = (*Synthesizer)(nil)
+
+type Synthesizer struct {
+	*Config
+	client *o
