0f4668386bdff3f9fcb392ba40840c1414320265
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-12-13 22:52:50 +0000

    Add models

--- pkg/provider/openai/transcriber.go
+++ pkg/provider/openai/transcriber.go
@@ -1,3 +1,19 @@
 package openai
 
 import (
+	"context"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+
+	"github.com/google/uuid"
+	"github.com/sashabaranov/go-openai"
+)
+
+var _ provider.Transcriber = (*Transcriber)(nil)
+
+type Transcriber struct {
+	*Config
+	client *openai.Client
+}
+
+func NewTr
