307f9814b171e5bb47b13fd200c7204682069e9c
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-12-19 18:20:44 +0000

    umem: fix up unplugging

--- pkg/provider/whisper/client.go
+++ pkg/provider/whisper/client.go
@@ -5,4 +5,19 @@ import (
 	"io"
 	"net/http"
 
-	"github.com
+	"github.com/adrianliechti/llama/pkg/provider"
+)
+
+type Client struct {
+	provider.Transcriber
+}
+
+func New(url string, options ...Option) (*Client, error) {
+	t, err := NewTranscriber(url, options...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		Transcriber: t
