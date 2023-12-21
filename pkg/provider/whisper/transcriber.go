6cbc2c04499de4c205ed6c1347f3738913ac14ed
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2023-12-21 14:31:13 +0000

    correct style

--- pkg/provider/whisper/transcriber.go
+++ pkg/provider/whisper/transcriber.go
@@ -1,4 +1,20 @@
 package whisper
 
 import (
-	"
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/google/uuid"
+)
+
+var (
+	_ provider.Transcriber = (*Transcriber)(nil)
+)
