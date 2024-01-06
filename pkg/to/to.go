2edd8ef59802af139c13113d8ecab413c2d0a23a
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-01-06 15:39:35 +0000

    Finished documentation

--- pkg/to/to.go
+++ pkg/to/to.go
@@ -4,4 +4,18 @@ func Ptr[T any](v T) *T {
 	return &v
 }
 
-func Keys
+func Keys[M ~map[K]V, K comparable, V any](m M) []K {
+	r := make([]K, 0, len(m))
+
+	for k := range m {
+		r = append(r, k)
+	}
+
+	return r
+}
+
+func Values[M ~map[K]V, K comparable, V any](m M) []V {
+	r := make([]V, 0, len(m))
+
+	for _, v := range m {
+		r = append(r, v)
