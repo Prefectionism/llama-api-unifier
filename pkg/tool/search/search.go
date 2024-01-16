46d2aa41fc78baf0705ce2501cc6a22a2ae2e536
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-01-16 18:38:08 +0000

    adding build script

--- pkg/tool/search/search.go
+++ pkg/tool/search/search.go
@@ -5,4 +5,19 @@ import (
 	"errors"
 
 	"github.com/adrianliechti/llama/pkg/index"
-	"github.com/adrianliechti/llama/pkg/jsonschema
+	"github.com/adrianliechti/llama/pkg/jsonschema"
+	"github.com/adrianliechti/llama/pkg/tool"
+)
+
+var _ tool.Tool = &Tool{}
+
+type Tool struct {
+	name        string
+	description string
+
+	index index.Provider
+}
+
+func New(index index.Provider) (*Tool, error) {
+	t := &Tool{
+		name:    
