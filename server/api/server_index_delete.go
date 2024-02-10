5ba1b90f61b8709f3fb95a938977e56c84595992
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-02-10 14:13:01 +0000

    Link updated

--- server/api/server_index_delete.go
+++ server/api/server_index_delete.go
@@ -1,4 +1,16 @@
 package api
 
 import (
-	"encoding/
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (s *Server) handleIndexDeletion(w http.ResponseWriter, r *http.Request) {
+	i, err := s.Index(chi.URLParam(r, "index"))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	
