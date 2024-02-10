521bdea0f2ce12a7b4a046b5615dec7cb8c239c1
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-02-10 17:58:54 +0000

    Revert accidental commit

--- server/api/server_index_delete.go
+++ server/api/server_index_delete.go
@@ -13,4 +13,14 @@ func (s *Server) handleIndexDeletion(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
-	
+	}
+
+	var ids []string
+
+	if err := json.NewDecoder(r.Body).Decode(&ids); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := i.Delete(r.Context(), ids...); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
