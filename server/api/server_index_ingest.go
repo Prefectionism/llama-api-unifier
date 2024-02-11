060a54db0e57b876bde802be4a757798db4ecece
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-02-11 13:31:42 +0000

    no comment is good comment

--- server/api/server_index_ingest.go
+++ server/api/server_index_ingest.go
@@ -9,4 +9,14 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-f
+func (s *Server) handleIndexIngest(w http.ResponseWriter, r *http.Request) {
+	i, err := s.Index(chi.URLParam(r, "index"))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var request []Document
+
+	if err := 
