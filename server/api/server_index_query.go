e75e4d44024f8377e0c678347f18244ed9e35008
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-02-14 06:24:19 +0000

    remove src

--- server/api/server_index_query.go
+++ server/api/server_index_query.go
@@ -10,4 +10,15 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (s *Server) handleIndexQuery(w http.ResponseWri
+func (s *Server) handleIndexQuery(w http.ResponseWriter, r *http.Request) {
+	i, err := s.Index(chi.URLParam(r, "index"))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var query Query
+
+	if err := json.NewDecoder(r.Body).Decode(&query); err != nil {
+		http.Error(w, e
