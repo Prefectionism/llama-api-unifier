3cf8d1624d446445462257aa2f7c575ae07b6a50
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-02-17 07:18:41 +0000

    Update debian files

--- server/api/server_translate.go
+++ server/api/server_translate.go
@@ -7,4 +7,15 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (s *Server) handleTranslate(w ht
+func (s *Server) handleTranslate(w http.ResponseWriter, r *http.Request) {
+	t, err := s.Translator(chi.URLParam(r, "translator"))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var request Document
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http
