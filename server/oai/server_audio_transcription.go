a48a527d60e580bdb50e65aaad7c9bb33c6df41a
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-02-25 19:57:14 +0000

    Upgrade bootstrap

--- server/oai/server_audio_transcription.go
+++ server/oai/server_audio_transcription.go
@@ -7,4 +7,18 @@ import (
 )
 
 func (s *Server) handleAudioTranscription(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseMultip
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	model := r.FormValue("model")
+
+	transcriber, err := s.Transcriber(model)
+
+	if err != nil {
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	prompt := r.FormValue("p
