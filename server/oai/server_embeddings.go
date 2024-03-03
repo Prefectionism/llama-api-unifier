06bdbb0e327993b143efcea1d89921e7de5fb680
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-03-03 14:07:42 +0000

    3.3.9

--- server/oai/server_embeddings.go
+++ server/oai/server_embeddings.go
@@ -9,4 +9,22 @@ func (s *Server) handleEmbeddings(w http.ResponseWriter, r *http.Request) {
 	var req EmbeddingsRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		writeError(w, http.StatusBadRequest
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	embedder, err := s.Embedder(req.Model)
+
+	if err != nil {
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var inputs []string
+
+	switch v := req.Input.(type) {
+	case string:
+		inputs = []string{v}
+	case []string:
+		inputs = v
+	}
