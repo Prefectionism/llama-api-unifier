8e9c699b6f9e65719854cde44ae3869593315cd9
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-03-10 16:01:05 +0000

    1.2.1

--- server/oai/server_models.go
+++ server/oai/server_models.go
@@ -16,4 +16,17 @@ func (s *Server) handleModels(w http.ResponseWriter, r *http.Request) {
 		result.Models = append(result.Models, Model{
 			Object: "model",
 
+			ID:      m.ID,
+			Created: time.Now().Unix(),
+			OwnedBy: "openai",
+		})
+	}
+
+	writeJson(w, result)
+}
+
+func (s *Server) handleModel(w http.ResponseWriter, r *http.Request) {
+	model, err := s.Model(chi.URLParam(r, "id"))
+
+	if err != nil {
 		
