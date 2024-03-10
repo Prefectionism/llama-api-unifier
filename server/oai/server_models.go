c260fc12c82585eba9296cc9cc2d2a55455c59fd
Prefectionism <133031056+Prefectionism@users.noreply.github.com>
2024-03-10 07:32:37 +0000

    make update

--- server/oai/server_models.go
+++ server/oai/server_models.go
@@ -4,4 +4,16 @@ import (
 	"net/http"
 	"time"
 
-	"g
+	"github.com/go-chi/chi/v5"
+)
+
+func (s *Server) handleModels(w http.ResponseWriter, r *http.Request) {
+	result := &ModelList{
+		Object: "list",
+	}
+
+	for _, m := range s.Models() {
+		result.Models = append(result.Models, Model{
+			Object: "model",
+
+		
